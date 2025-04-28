9b5de2ebfa8090807017b6cbc7c41fa462228439
Jasmin May Anderson <05jasmay@gmail.com>
2025-04-28 14:27:19 +0200

    Added underscore functionality

--- Hangman/Hangman/Program.cs
+++ Hangman/Hangman/Program.cs
@@ -15,12 +15,13 @@ namespace Hangman
             //List which holds the different words
             List<string> words = new List<string>();
 
-            words.Add("Horse");
-            words.Add("Computer");
-            words.Add("Iphone");
+            words.Add("horse");
+            words.Add("computer");
+            words.Add("iphone");
 
             //Variables for the selected word, as well as the length of the word
             string chosenWord = words[random.Next(words.Count)];
+                //Console.WriteLine(chosenWord);
             int lengthOfWord = chosenWord.Length;
 
             //string __ = new string(char, int);
@@ -29,7 +30,30 @@ namespace Hangman
             Console.WriteLine($"The chosen word has: {underscore} words!");
             //Console.WriteLine(chosenWord + "" + lengthOfWord);
 
-            //While loop;
-        }
+            //While loop to check if there is an underscore
+            while (underscore.Contains('_'))
+            { 
+                List<char> lettersTried = new List<char>();
+
+                Console.WriteLine("Enter a letter: ");
+                char letter = Convert.ToChar(Console.ReadLine());
+
+                for(int i = 0; i < lengthOfWord; i++)
+                {
+                    if (chosenWord[i] == letter)
+                    {
+                        underscore = underscore.Remove(i, 1).Insert(i, letter.ToString());
+						Console.WriteLine($"Current word: {underscore}");
+					}
+                }
+
+				if (!chosenWord.Contains(letter))
+				{
+					lettersTried.Add(letter);
+					Console.WriteLine("Letter is not in the word! Try again\n");
+				}
+			}
+            Console.WriteLine("You have completed the game");
+		}
     }
 }
